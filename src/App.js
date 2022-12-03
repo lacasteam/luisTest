fb310c5f465481f6782a6ccaa4c19f1d3efccf75
Christian Lacaste <christianlacaste28@gmail.com>
2022-12-03 13:15:27 +0800

    added slash on App.js home route

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       </div>
       <div>
          <Routes>
-          <Route path="" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/features" element={<Features/>}/>
           <Route path="/prices" element={<Prices/>}/>
           <Route path="/students" element={<Student/>}/>
